ef6296c7f03b2f7c4a374429df7b77722bebdb02
Lesly Sandoval <lesly.sandoval@bluefletch.com>
2019-09-20 10:12:46 -0400

    first commit

---
+++ PlatformTest/app/src/main/java/com/thd/platformtest/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.thd.platformtest
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
